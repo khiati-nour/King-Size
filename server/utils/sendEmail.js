8df79ab21924604945691328793f0f855a2c4048
khiati-nour <khiatihouda@gmail.com>
2021-04-19 09:18:09 +0200

    edit .config

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -2,10 +2,10 @@ import nodemailer from 'nodemailer';
 
 const sendEmail = (options)  => {
     const transporter = nodemailer.createTransport({
-        service: process.env.EMAIL_SERVICE || 'SendGrid',
+        service: process.env.EMAIL_SERVICE ,
         auth:{
-          user: process.env.EMAIL_USERNAME || 'apikey',
-          pass: process.env.EMAIL_PASSWORD || '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'   
+          user: process.env.EMAIL_USERNAME, 
+          pass: process.env.EMAIL_PASSWORD     
         }
     })
     const mailOptions = {
